cc23853205c54142d5cd1ca0cd2009441465259f
younghun.chung <younghun.chung@projectwith.io>
2025-06-20 11:30:43 +0900

    [Fix] Exception Handling Error Modified

--- src/main/java/com/pinkcat/quick_reserve_seller/config/GlobalResponseAdvice.java
+++ src/main/java/com/pinkcat/quick_reserve_seller/config/GlobalResponseAdvice.java
@@ -1,13 +1,20 @@
 package com.pinkcat.quick_reserve_seller.config;
 
+import com.pinkcat.quick_reserve_seller.common.exceptions.PinkCatException;
 import com.pinkcat.quick_reserve_seller.common.model.BaseResponse;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.LinkedHashMap;
 
 @RestControllerAdvice
 public class GlobalResponseAdvice implements ResponseBodyAdvice<Object> {
@@ -17,15 +24,17 @@ public class GlobalResponseAdvice implements ResponseBodyAdvice<Object> {
     }
 
     @Override
-    public Object beforeBodyWrite(
-            Object body,
-            MethodParameter returnType,
-            MediaType selectedContentType,
-            Class<? extends HttpMessageConverter<?>> selectedConverterType,
-            ServerHttpRequest request,
-            ServerHttpResponse response
-    ) {
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof LinkedHashMap<?, ?> && ((LinkedHashMap<String, ?>) body).containsKey("error")) return body;
+
         if (body instanceof BaseResponse) return body;
+
         return new BaseResponse<>(body);
+    }
+
+    @ExceptionHandler(PinkCatException.class)
+    public ResponseEntity<BaseResponse<String>> handlePinkCatException(PinkCatException ex, WebRequest request) {
+        BaseResponse<String> response = new BaseResponse<>(ex);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }
