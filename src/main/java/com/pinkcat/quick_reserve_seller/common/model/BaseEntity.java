383873851b254ca4609a6a6a06cf91097633f6ff
younghun.chung <younghun.chung@projectwith.io>
2025-06-20 16:46:00 +0900

    [Refactor] BaseEntity Delombok

--- src/main/java/com/pinkcat/quick_reserve_seller/common/model/BaseEntity.java
+++ src/main/java/com/pinkcat/quick_reserve_seller/common/model/BaseEntity.java
@@ -1,10 +1,6 @@
 package com.pinkcat.quick_reserve_seller.common.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -15,10 +11,6 @@ import java.time.Instant;
 import java.util.Objects;
 
 @MappedSuperclass
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
 
@@ -35,6 +27,9 @@ public abstract class BaseEntity implements Serializable {
     @Column(columnDefinition = "boolean default true")
     private Boolean active = true;
 
+    public BaseEntity() {
+    }
+
     @Override
     public final boolean equals(Object o) {
         if (this == o) return true;
@@ -49,5 +44,41 @@ public abstract class BaseEntity implements Serializable {
     @Override
     public final int hashCode() {
         return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    public Long getPk() {
+        return this.pk;
+    }
+
+    public void setPk(Long pk) {
+        this.pk = pk;
+    }
+
+    public Long getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String toString() {
+        return "BaseEntity(pk=" + this.getPk() + ", createdAt=" + this.getCreatedAt() + ", updatedAt=" + this.getUpdatedAt() + ", active=" + this.getActive() + ")";
     }
 }
