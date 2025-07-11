3ae2043b2c1fee5043230e218e3afe3137400d35
younghun.chung <younghun.chung@projectwith.io>
2025-07-11 09:14:51 +0900

    [Refactor] Price, DiscountPrice Valid Refactoring

--- src/main/kotlin/com/pinkcat/quick_reserve_seller/product/service/ProductServiceImpl.kt
+++ src/main/kotlin/com/pinkcat/quick_reserve_seller/product/service/ProductServiceImpl.kt
@@ -148,10 +148,10 @@ class ProductServiceImpl(
             if (categoryRepository.existsByTopCategoryPkAndActive(categoryPk, true))
                 throw CategoryNotTopCategoryException("]-----] ProductServiceImpl::productReqValidCheck Category Have Leaf(req: $req) [-----[")
         }
-        if (req.price < 0 || (req.stock != null && req.stock < 0))
+        if (req.price <= 0 || (req.stock != null && req.stock < 0))
             throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
         if (req.discount != null) {
-            if (req.discount.price >= req.price) throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
+            if (req.discount.price <= 0 || req.discount.price >= req.price) throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
             if (req.discount.startAt != null && req.discount.startAt < Instant.now().toEpochMilli())
                 throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
             if (req.discount.endAt != null && req.discount.startAt != null && req.discount.endAt < req.discount.startAt)
