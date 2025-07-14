72cbd65dcee2b56d4a67784a7c72bd665d456239
younghun.chung <younghun.chung@projectwith.io>
2025-07-14 15:40:22 +0900

    [Fix] Delete Product Error Fix

--- src/main/kotlin/com/pinkcat/quick_reserve_seller/product/service/ProductServiceImpl.kt
+++ src/main/kotlin/com/pinkcat/quick_reserve_seller/product/service/ProductServiceImpl.kt
@@ -125,7 +125,7 @@ class ProductServiceImpl(
 
     @Transactional
     override fun deleteProduct(sellerPk: Long, productPk: Long): Boolean {
-        val product = productRepository.findByPkAndActive(sellerPk, true)
+        val product = productRepository.findByPkAndActive(productPk, true)
             .orElseThrow { ProductNotFoundException("]-----] ProductServiceImpl::findProduct Product Not Found(productPk: $productPk) [-----[") }
 
         if (product.seller.pk != sellerPk)
