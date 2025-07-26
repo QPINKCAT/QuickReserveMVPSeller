2c8cfe3e02d0107851368385e09c51575972f901
hsilnam <hsilnam3@gmail.com>
2025-07-27 07:06:00 +0900

    feat: sellerEntity id, store 필드 추가, id값을 통한 데이터 찾는 메서드 추가

--- src/main/kotlin/com/pinkcat/quick_reserve_seller/seller/repository/SellerRepository.kt
+++ src/main/kotlin/com/pinkcat/quick_reserve_seller/seller/repository/SellerRepository.kt
@@ -2,6 +2,8 @@ package com.pinkcat.quick_reserve_seller.seller.repository
 
 import com.pinkcat.quick_reserve_seller.common.repository.ActiveRepository
 import com.pinkcat.quick_reserve_seller.seller.entity.SellerEntity
+import java.util.*
 
 interface SellerRepository : ActiveRepository<SellerEntity, Long> {
+    fun findById(userId: String): Optional<SellerEntity>
 }
