29e70e284f46975423a3a52693f7df79e68eab73
younghun.chung <younghun.chung@projectwith.io>
2025-08-04 15:48:46 +0900

    [Style] ProductValidator Error Message Modified

--- src/main/kotlin/com/pinkcat/quick_reserve_seller/product/validation/ProductValidator.kt
+++ src/main/kotlin/com/pinkcat/quick_reserve_seller/product/validation/ProductValidator.kt
@@ -23,16 +23,16 @@ class ProductValidator(
     private fun reqValidCheck(req: ProductReq) {
         req.categoryPks.forEach { categoryPk ->
             if (!categoryRepository.existsByPkAndActive(categoryPk, true))
-                throw CategoryNotFoundException("]-----] ProductServiceImpl::productReqValidCheck Category Not Found(req: $req) [-----[")
+                throw CategoryNotFoundException("]-----] ProductValidator::productReqValidCheck Category Not Found(req: $req) [-----[")
             if (categoryRepository.existsByTopCategoryPkAndActive(categoryPk, true))
-                throw CategoryNotTopCategoryException("]-----] ProductServiceImpl::productReqValidCheck Category Have Leaf(req: $req) [-----[")
+                throw CategoryNotTopCategoryException("]-----] ProductValidator::productReqValidCheck Category Have Leaf(req: $req) [-----[")
         }
         if (req.discount != null) {
-            if (req.discount.price <= 0 || req.discount.price >= req.price) throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
+            if (req.discount.price <= 0 || req.discount.price >= req.price) throw ProductReqInvalidException("]-----] ProductValidator::productReqValidCheck Product Request Invalid(req: $req) [-----[")
             if (req.discount.startAt != null && req.discount.startAt < Instant.now().toEpochMilli())
-                throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
+                throw ProductReqInvalidException("]-----] ProductValidator::productReqValidCheck Product Request Invalid(req: $req) [-----[")
             if (req.discount.endAt != null && req.discount.startAt != null && req.discount.endAt < req.discount.startAt)
-                throw ProductReqInvalidException("]-----] ProductServiceImpl::productReqValidCheck Product Request Invalid(req: $req) [-----[")
+                throw ProductReqInvalidException("]-----] ProductValidator::productReqValidCheck Product Request Invalid(req: $req) [-----[")
         }
     }
 }
