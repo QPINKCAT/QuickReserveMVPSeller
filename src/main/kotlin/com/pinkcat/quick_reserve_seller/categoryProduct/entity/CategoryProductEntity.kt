3a520c28cac91ef62ca9735e32ccc9c6f06b324a
younghun.chung <younghun.chung@projectwith.io>
2025-08-08 09:45:55 +0900

    [Refactor] Category Product Entity Column Name Modified

--- src/main/kotlin/com/pinkcat/quick_reserve_seller/categoryProduct/entity/CategoryProductEntity.kt
+++ src/main/kotlin/com/pinkcat/quick_reserve_seller/categoryProduct/entity/CategoryProductEntity.kt
@@ -3,15 +3,13 @@ package com.pinkcat.quick_reserve_seller.categoryProduct.entity
 import com.pinkcat.quick_reserve_seller.category.Entity.CategoryEntity
 import com.pinkcat.quick_reserve_seller.common.model.BaseEntity
 import com.pinkcat.quick_reserve_seller.product.entity.ProductEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import lombok.Data
 
 @Entity
 @Data
 @Table(name = "category_product")
+@AttributeOverride(name = "pk", column = Column(name = "category_product_pk"))
 class CategoryProductEntity(
     @ManyToOne(fetch = FetchType.LAZY)
     val category: CategoryEntity,
